c7aec6216cec4c2c60d6e8c9111542426c359b20
吴展图 <326862309@qq.com>
2020-05-13 07:41:49 +0800

    add new file again

---
+++ Example/TuGoodsDetail/WUSecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  WUSecondViewController.h
+//  TuGoodsDetail_Example
+//
+//  Created by 吴展图 on 2020/5/13.
+//  Copyright © 2020 wuzhantu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WUSecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/TuGoodsDetail/WUSecondViewController.m
@@ -0,0 +1,32 @@
+//
+//  WUSecondViewController.m
+//  TuGoodsDetail_Example
+//
+//  Created by 吴展图 on 2020/5/13.
+//  Copyright © 2020 wuzhantu. All rights reserved.
+//
+
+#import "WUSecondViewController.h"
+
+@interface WUSecondViewController ()
+
+@end
+
+@implementation WUSecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
